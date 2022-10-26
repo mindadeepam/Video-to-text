c939d05e40944d9d6732063d26481d62c004c032
mindadeepam-dn <minda.deepam@doubtnut.com>
2022-10-27 04:22:07 +0530

    updated shell files

--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # add brew to path
 # export BREW_HOME="/home/linuxbrew/.linuxbrew/bin"
@@ -12,20 +12,28 @@ test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
 
 #install wget
+echo -e "\n\n installing wget"
 brew install wget
+echo -e "\n\n installed wget"
 
 # install python specific version (3.6.4)
-chmod u+x install_python.sh
-apt install make gcc zlib1g-dev libffi-dev
-./install_python.sh 3.6.4
-
+echo -e "\n\n installing python"
+# chmod u+x install_python.sh
+# apt install make gcc zlib1g-dev libffi-dev
+# ./install_python.sh 3.6.
+sudo apt-get install python3.6
+echo -e "\n\n python installed"
 
 # install whisper
+echo -e "\n\n installing whisper"
 pip install git+https://github.com/openai/whisper.git
 echo -e "\n\nwhisper cloned"
+echo -e "\n\n installed whisper"
 
 # install ffmpeg
+echo -e "\n\n installing ffmpeg"
 brew install ffmpeg
+echo -e "\n\n installed ffmpeg"
 
 
 filepath=""
@@ -46,6 +54,10 @@ done
 
 echo -e "\nrunning main.py..."
 
-python main.py \
-  --filepath=${filepath} \
-  --outputdir=${outputdir} \
+# python main.py \
+#   --filepath=${filepath} \
+#   --outputdir=${outputdir} \
+  
+  
+  
+  
